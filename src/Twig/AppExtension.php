caec233ad9a883e65c3cd6f4ab9699d44b7dbd48
kamal tahir <tahirekamal@gmail.com>
2019-03-13 23:24:41 +0100

    updates

---
+++ src/Twig/AppExtension.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Twig;
+
+use Twig\Extension\AbstractExtension;
+use Twig\TwigFilter;
+use Twig\TwigFunction;
+
+class AppExtension extends AbstractExtension
+{
+    public function getFilters(): array
+    {
+        return [
+            // If your filter generates SAFE HTML, you should add a third
+            // parameter: ['is_safe' => ['html']]
+            // Reference: https://twig.symfony.com/doc/2.x/advanced.html#automatic-escaping
+            new TwigFilter('cached_markdown', [$this, 'processMarkdown'], ['is_safe' => ['html']] ),
+        ];
+    }
+
+
+    public function doSomething($value)
+    {
+        // ...
+    }
+
+    public function processMarkdown($value)
+    {
+        return strtoupper($value);
+    }
+    
+
+}
